b8514861bb41780c1aa55451be6fa18985462b35
Elias Dalan <cedalan@ntnu.no>
2024-11-25 13:12:29 +0100

    Added possibility to change nickname

--- src/main/java/com/elias/qualityoflifeplus/QualityOfLifePlusPlugin.java
+++ src/main/java/com/elias/qualityoflifeplus/QualityOfLifePlusPlugin.java
@@ -9,6 +9,7 @@ import com.elias.qualityoflifeplus.blb.BlbListener;
 import com.elias.qualityoflifeplus.blb.BlbTabCompleter;
 import com.elias.qualityoflifeplus.events.PlayerChangedItemInHandListener;
 import com.elias.qualityoflifeplus.events.PlayerLogoutListener;
+import com.elias.qualityoflifeplus.extras.ExtrasCommandHandler;
 import com.elias.qualityoflifeplus.utils.PlayerDataManager;
 
 public class QualityOfLifePlusPlugin extends JavaPlugin {
@@ -43,6 +44,11 @@ public class QualityOfLifePlusPlugin extends JavaPlugin {
 
         //Enable command completer for blb
         this.getCommand("blb").setTabCompleter(new BlbTabCompleter());
+
+        //Extras section
+
+        //Nickname command:
+        this.getCommand("changenick").setExecutor(new ExtrasCommandHandler());
     }
 
     @Override
--- src/main/java/com/elias/qualityoflifeplus/extras/ExtrasCommandHandler.java
+++ src/main/java/com/elias/qualityoflifeplus/extras/ExtrasCommandHandler.java
@@ -3,13 +3,34 @@ package com.elias.qualityoflifeplus.extras;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class ExtrasCommandHandler implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'onCommand'");
+        switch (command.getName().toLowerCase()) {
+            case "changenick":
+            handleChangeNick(sender, args);
+        }
+        
+        return true;
+    }
+
+    public void handleChangeNick(CommandSender sender, String[] args) {
+        if (args.length < 1) {
+            sender.sendMessage("You need to provide a nickname!");
+        }
+
+        String newName = args[0];
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            player.setDisplayName(newName);
+            player.sendMessage("You changed your name to " + newName + "!");
+        } else {
+            sender.sendMessage("Only players can change their names!");
+        }
     }
     
 }
