90b4c6deaa2e817fed89d0286dcc15713faaedf5
Elias Dalan <cedalan@ntnu.no>
2024-11-25 13:00:59 +0100

    Added extras and skeleton for ExtrasCommandHandler. Updated plugins.yml to include nickname

---
+++ src/main/java/com/elias/qualityoflifeplus/extras/ExtrasCommandHandler.java
@@ -0,0 +1,15 @@
+package com.elias.qualityoflifeplus.extras;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ExtrasCommandHandler implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onCommand'");
+    }
+    
+}
